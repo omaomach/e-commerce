3553a08bfe379a1fd6ca647ad580ebcbbecb2f23
kapadokia <ktnyandoch@gmail.com>
2022-09-29 09:12:22 +0300

    created an add products page

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -1,83 +1,16 @@
 // admin
 import React, {useState, useEffect} from "react";
-import Container from './Container';
-import { 
-  Table,
-  Avatar, 
-  Spin
-
-} from "antd";
-import 'antd/dist/antd.css';
-// import the client 
-import {getAllProducts} from './client'; 
+import AddProduct from "./addProduct";
 import Banner from "./banner";
+import ProductTable from "./productTable";
 
 export default function Admin(){
-
-    // initialize state
-    const [products, setProducts] =useState([])
-
-    // fetch data
-    useEffect(()=>{
-        getAllProducts()
-        .then(res =>res.json())
-        .then(data =>{
-          setProducts(data)
-        })
-    }, [])
-
-
-    // create a datasource for antd
-    //first check if products exists
-    
-        const objects = [
-            
-            {
-                title:'Number', 
-                dataIndex:'id',
-                key:'id'
-                },
-            {
-              title:'Product Image',
-              key:'avatar',
-              render:(text, product)=>(
-                <Avatar size='large' src={product.image} /> 
-              )
-            },
-        
-            {
-              title:'Title', 
-              dataIndex:'title',
-              key:'title'
-            },
-            {
-              title:'Description', 
-              dataIndex:'description',
-              key:'description'
-            },
-            {
-              title:'Price', 
-              dataIndex:'price',
-              key:'price'
-            }, 
-            {
-              title:'Category', 
-              dataIndex:'category',
-              key:'category'
-            }
-          ];
-    
-
   
     return(
         <>
         <Banner />
-        <Container>
-        <Table dataSource={products} 
-             columns={objects} 
-             rowKey='id' 
-             />
-        </Container>
+        <AddProduct />
+        <ProductTable />
         </>
        
     )
