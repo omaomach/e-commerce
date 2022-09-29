0fd5a32e16c2b4d231327bd05d8a6b33b8080873
Rajab <rkarume@gmail.com>
2022-09-29 15:37:46 +0300

    Global folder added

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,1 +1,11 @@
 // home
+import React from "react";
+// import Banner from "../admin/banner";
+import 'antd/dist/antd.css';
+import Homebanner from "./homeBanner";
+
+export default function Home(){
+
+    return (
+        < Homebanner />)
+}
