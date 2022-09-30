9a15348fff0bc5f6af96ad1047bffd869784ca4c
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-09-30 07:32:55 +0300

    created fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Admin from './components/admin/admin';
-
+import Prd from './components/products/Prd';
 function App() {
   return (
-   <Admin/>
+    <>
+      <Prd />
+      <Admin />
+    </>
   );
 }
 
